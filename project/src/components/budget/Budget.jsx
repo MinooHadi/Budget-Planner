556e46ca7abfaee6ea3c1de63d7654f8c1f725a4
Minoo <minoo.hadi2904@gmail.com>
2023-02-09 20:34:35 +0330

    feat: create budget component

---
+++ project/src/components/budget/Budget.jsx
@@ -0,0 +1,12 @@
+function Budget() {
+    return(
+        <div>
+            <div>
+                <p>Budget:</p> <span>$2000</span>
+            </div>
+            <button>edit</button>
+        </div>
+    )
+}
+
+export default Budget
