e2ea53321b2d357420d1ba086c2fc43292433963
Minoo <minoo.hadi2904@gmail.com>
2023-02-11 17:06:22 +0330

    feat: set budget and editable budget

--- project/src/components/budget/Budget.jsx
+++ project/src/components/budget/Budget.jsx
@@ -1,15 +1,45 @@
+import { useContext, useEffect, useRef } from "react";
 import Button from "../button/Button";
+import { store } from "../context/Context";
 import "./../../assets/styles/budget.css";
 
 function Budget() {
-    return(
-        <div className="budget-main">
-            <div className="budget-desc">
-                <p>Budget: <span>$2000</span></p> 
-            </div>
-            <Button text="Edit"/>
-        </div>
-    )
+  const { editBudget, setEditBudget, budget, setBudget } = useContext(store);
+
+  const expense = useRef(null);
+
+  function editBudgetBtn() {
+    setEditBudget(true);
+  }
+
+  useEffect(() => {
+    expense.current.focus();
+  }, [editBudget]);
+
+  function saveBudgetBtn() {
+    setEditBudget(false);
+    setBudget(expense.current.innerText);
+  }
+
+  return (
+    <div className="budget-main">
+      <div className="budget-desc">
+        <p>Budget: $</p>
+        {editBudget ? (
+          <span ref={expense} contentEditable>
+            {budget}
+          </span>
+        ) : (
+          <span ref={expense}> {budget} </span>
+        )}
+      </div>
+      {editBudget ? (
+        <Button text="Save" onClick={saveBudgetBtn} />
+      ) : (
+        <Button text="Edit" onClick={editBudgetBtn} />
+      )}
+    </div>
+  );
 }
 
-export default Budget
+export default Budget;
--- project/src/components/context/Context.jsx
+++ project/src/components/context/Context.jsx
@@ -4,14 +4,22 @@ import React, { useState } from "react";
 export const store = React.createContext({
   addBudget: [],
   setAddBudget: () => {},
+  editBudget: false,
+  setEditBudget: () => {},
+  budget: 2000,
+  setBudget: () => {}
 });
 
 // provider
 const ContextProvider = ({ children }) => {
   const [addBudget, setAddBudget] = useState([]);
+  const [editBudget, setEditBudget] = useState(false);
+  const [budget, setBudget] = useState(2000)
 
   return (
-    <store.Provider value={{ addBudget, setAddBudget }}>
+    <store.Provider
+      value={{ addBudget, setAddBudget, editBudget, setEditBudget, budget, setBudget }}
+    >
       {children}
     </store.Provider>
   );
